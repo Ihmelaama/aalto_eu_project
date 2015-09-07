a8cae467f94f63536523100add28a12dbbaed239
Tove <tove.brant@gmail.com>
2015-09-07 11:26:33 +0300

    valmistelen mission tekstien muuttamista koodista

--- Assets/Scripts/UI/MissionMenu.cs
+++ Assets/Scripts/UI/MissionMenu.cs
@@ -9,6 +9,18 @@ public class MissionMenu : MonoBehaviour {
     bool missionVisible = true;
 
 
+    //easy references to the mission information
+    public Text mission_1_name;
+    public Text mission_1_desc;
+
+    public Text mission_2_name;
+    public Text mission_2_desc;
+
+    public Text mission_3_name;
+    public Text mission_3_desc;
+
+
+
     void Start()
     {
         missionBG.rectTransform.sizeDelta = new Vector2(missionBG.rectTransform.rect.width, Screen.height/1.5f);
@@ -29,6 +41,28 @@ public class MissionMenu : MonoBehaviour {
             missionBG.gameObject.SetActive(false);
             missionButton.gameObject.transform.localPosition = new Vector3(0, 0);
             missionVisible = true;
+        }
+    }
+
+    public void ChangeMissionName(string newName, int missionNumber)
+    {
+        switch (missionNumber)
+        {
+            case 1: mission_1_name.text = newName; break;
+            case 2: mission_2_name.text = newName; break;
+            case 3: mission_3_name.text = newName; break;
+            default: Debug.Log("Check the mission number!"); break;
+        }
+    }
+
+    public void ChangeMissionDesc(string newDesc, int missionNumber)
+    {
+        switch (missionNumber)
+        {
+            case 1: mission_1_desc.text = newDesc; break;
+            case 2: mission_2_desc.text = newDesc; break;
+            case 3: mission_3_desc.text = newDesc; break;
+            default: Debug.Log("Check the mission number!"); break;
         }
     }
 }
