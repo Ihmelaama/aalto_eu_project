c621ecdbe9a48ee78a2026be7c06ec080d7b4572
Tove <tove.brant@gmail.com>
2015-10-01 16:07:45 +0300

    missionkoodia aloiteltu

---
+++ Assets/Scripts/Mission/MissionManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissionManager : MonoBehaviour {
+
+
+    public static List<Mission> missions;
+    public List<Mission> publicMissionList;
+
+    void Start()
+    {
+        missions = new List<Mission>();
+        foreach(Mission mis in publicMissionList)
+        {
+            missions.Add(mis);
+        }
+    }
+
+    public static void changeMissionStatus(string MissionName)
+    {
+        Mission currentMission = null;
+        foreach (Mission mis in missions)
+        {
+            if (mis.name.Equals(MissionName)){
+                currentMission = mis;
+            }
+        }
+        currentMission.amountItemsNeeded--;
+        if (currentMission.missionDone)
+        {
+            bool doneHere = false;
+            foreach (Mission mis in missions)
+            {
+                doneHere = mis.missionDone;
+            }
+            if (doneHere)
+            {
+                //we are so done here
+            }
+            //make UI changes
+        }
+    }
+
+}
+
+
+public class Mission
+{
+        public string name;
+        public int amountItemsNeeded;
+        public int itemIDNeeded;
+
+        public bool missionDone { get{ return (amountItemsNeeded <= 0); }}
+}
