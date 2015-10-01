d794da40742cdd72e93eb11d0774a5e952995842
Tove <tove.brant@gmail.com>
2015-10-01 15:28:53 +0300

    intro gridi toimii

--- Assets/Scripts/CharacterSelection/CharSelectScript.cs
+++ Assets/Scripts/CharacterSelection/CharSelectScript.cs
@@ -11,9 +11,9 @@ public class CharSelectScript : MonoBehaviour {
     public GameObject[] buttons2;
     public GameObject[] buttons3;
 
-    public Canvas Canvas;
-	public Canvas Canvas2;
-	public Canvas Canvas3;
+    public GameObject Canvas;
+	public GameObject Canvas2;
+	public GameObject Canvas3;
 
 
 	//private
@@ -36,17 +36,17 @@ public class CharSelectScript : MonoBehaviour {
 		//create a button
 		for(int x = buttons.Length-1; x>=0; x--){
 			button = Instantiate (buttons[x], new Vector3 (0, 0), Quaternion.identity) as GameObject;
-			button.transform.SetParent(Canvas.transform);
+			button.transform.SetParent(Canvas.transform, false);
 		}
 
 		for(int x = buttons2.Length-1; x>=0; x--){
 			button = Instantiate (buttons2[x], new Vector3 (0, 0), Quaternion.identity) as GameObject;
-			button.transform.SetParent(Canvas2.transform);
+			button.transform.SetParent(Canvas2.transform, false);
 		}
 
 		for(int x = buttons3.Length-1; x>=0; x--){
 			button = Instantiate (buttons3[x], new Vector3 (0, 0), Quaternion.identity) as GameObject;
-			button.transform.SetParent(Canvas3.transform);
+			button.transform.SetParent(Canvas3.transform, false);
 		}
 
 	}
