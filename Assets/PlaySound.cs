1615970d711eebfa7ce8dcaa4030a0e42c2a51da
velilaamanen <kitaraopetusta@gmail.com>
2015-10-02 00:12:20 +0300

    Fabric, test sounds & sound testing scene

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Playsound (string Eventname) {
+
+		Fabric.EventManager.Instance.PostEvent (Eventname);
+	
+	}
+}
---
+++ Assets/SoundTestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTestScript : MonoBehaviour {
+
+	public string T;
+	public string Y;
+	public string U;
+
+
+	void Update () {
+		//For testing Fabric sound levels quickly
+
+		if (Input.GetKeyDown (KeyCode.T)) {
+			Fabric.EventManager.Instance.PostEvent(T);
+		}
+		if (Input.GetKeyDown (KeyCode.Y)) {
+			Fabric.EventManager.Instance.PostEvent(Y);
+		}
+		if (Input.GetKeyDown (KeyCode.U)) {
+			Fabric.EventManager.Instance.PostEvent(U);
+		}
+
+	}
+}
